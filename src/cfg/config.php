0760123396dc7e32a91ab440772cb53b341cbab4
Patrick Doudy <patrick.doudy@gmail.com>
2019-04-24 21:22:53 -0600

    fixed hard coded log path in config

--- src/cfg/config.php
+++ src/cfg/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define("ERROR_LOG_PATH", 		"/Users/patdoudy/simfin/log/errorLog.txt");
+define("ERROR_LOG_PATH", 		dirname(__FILE__,3)."/log/errorLog.txt");
 define("ERROR_LOG_LINES", 		1024);
-define("ACTIVITY_LOG_PATH", 	"/Users/patdoudy/simfin/log/activityLog.txt");
+define("ACTIVITY_LOG_PATH", 	dirname(__FILE__,3)."/log/activityLog.txt");
 define("ACTIVITY_LOG_LINES",	1024);
 
 ?>
